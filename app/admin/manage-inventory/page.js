a7a41520b89f02a8d9935135f1f912e321e1d51f
afkmary <maryannegarcia07@yahoo.com>
2026-03-29 23:47:44 -0600

    REDO

--- app/admin/manage-inventory/page.js
+++
@@ -1,8 +0,0 @@
-export default function ManageInventoryPage() {
-  return (
-    <main style={{ padding: "2rem" }}>
-      <h1>Manage Inventory</h1>
-      <p>Coming soon...</p>
-    </main>
-  );
-}
